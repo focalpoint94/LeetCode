7a564aca196da168ec661fb5d6247c9df9133c10
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2021-12-19 23:10:06 +0900

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- 236. Lowest Common Ancestor of a Binary Tree.py
+++ 236. Lowest Common Ancestor of a Binary Tree.py
@@ -7,36 +7,15 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.ret = None
-        self.dfs(root, p, q)
-        return self.ret
-    
-    def dfs(self, node, p, q):
-        if node == None:
-            return None
-        
-        l = self.dfs(node.left, p, q) if node.left else None
-        r = self.dfs(node.right, p, q) if node.right else None
-        if (l == True) or (r == True):
-            return True
-            
-        if node == p:
-            if l == q or r == q:
-                self.ret = node
-                return True
-            else:
-                return p
-        elif node == q:
-            if l == p or r == p:
-                self.ret = node
-                return True
-            else:
-                return q
-        elif (l == p and r == q) or (l == q and r == p):
-            self.ret = node
-            return True
-        elif l == p or l == q:
-            return l
-        elif r == p or r == q:
-            return r
-        return None
+        if root == p or root == q:
+            return root
+        left = right = None
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        if left:
+            return left
+        return right
