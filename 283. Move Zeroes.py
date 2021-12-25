f4a4ef5c72e12e269a54dcf2834e8802c5d3716e
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2021-12-25 22:14:40 +0900

    Create 283. Move Zeroes.py

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i, num in enumerate(nums):
+            if num != 0:
+                nums[j] = num
+                j += 1
+        for k in range(j, len(nums)):
+            nums[k] = 0
+        
