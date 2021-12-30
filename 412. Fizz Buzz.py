35b3788d5493c8766adc3218f73b99e91bfeee73
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2021-12-30 22:15:19 +0900

    Create 412. Fizz Buzz.py

---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ret = []
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                ret.append("FizzBuzz")
+            elif i % 3 == 0:
+                ret.append("Fizz")
+            elif i % 5 == 0:
+                ret.append("Buzz")
+            else:
+                ret.append(str(i))
+        return ret
