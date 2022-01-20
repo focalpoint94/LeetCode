aca954d91b9deae591fad367154a96040b096e08
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-01-20 14:44:26 +0900

    Update 65. Valid Number.py

--- 65. Valid Number.py
+++ 65. Valid Number.py
@@ -3,3 +3,34 @@ class Solution:
         import re
         pat = re.compile('(^[+-]?((\d+(\.\d*)?)|(\.\d+))$)|(^[+-]?((\d+(\.\d*)?)|(\.\d+))[eE][+-]?\d+$)')
         return re.match(pat, s)
+
+
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        dfa = [
+            {"sign": 2, "dot": 3, "digit": 1},
+            {"digit": 1, "expo": 5, "dot": 4},
+            {"digit": 1, "dot":3},
+            {"digit": 4},
+            {"digit": 4, "expo": 5},
+            {"sign": 6, "digit": 7},
+            {"digit": 7},
+            {"digit": 7},
+        ]
+        current_state =0
+        for char in s:
+            typ = ''
+            if char.isdigit():
+                typ = 'digit'
+            elif char == '.':
+                typ = 'dot'
+            elif char in ['e', 'E']:
+                typ = 'expo'
+            elif char in ['+', '-']:
+                typ = 'sign'
+            else:
+                return False
+            if typ not in dfa[current_state]:
+                return False
+            current_state = dfa[current_state][typ]
+        return current_state in [1, 4, 7]
