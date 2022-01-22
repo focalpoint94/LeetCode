6d30ad45cff742e94982798782ca62ba2c57a761
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-01-22 17:50:11 +0900

    Update 212. Word Search II.py

--- 212. Word Search II.py
+++ 212. Word Search II.py
@@ -1,32 +1,39 @@
 class Solution:
     def findWords(self, board, words):
+        self.ret = []
         self.trie = {}
         for word in words:
             t = self.trie
-            for char in word:
-                if char not in t:
-                    t[char] = {}
-                t = t[char]
+            for c in word:
+                if c not in t:
+                    t[c] = {}
+                t = t[c]
             t['#'] = '#'
-        self.ret = []
-        for y in range(len(board)):
-            for x in range(len(board[0])):
-                self.dfs(self.trie, board, y, x, '')
+        
+        m, n = len(board), len(board[0])
+        for y in range(m):
+            for x in range(n):
+                self.dfs(board, self.trie, y, x, '')
         return self.ret
     
-    def dfs(self, t, board, y, x, path):
-        if '#' in t:
-            self.ret.append(path)
-            t.pop('#')
-        if y < 0 or y >= len(board) or x < 0 or x >= len(board[0]):
+    def dfs(self, board, node, y, x, word):
+        if '#' in node:
+            del node['#']
+            self.ret.append(word)
+        
+        if 0 > y or y >= len(board) or 0 > x or x >= len(board[0]):
             return
+        
         char = board[y][x]
-        if char in t:
-            board[y][x] = '#'
-            self.dfs(t[char], board, y, x+1, path+char)
-            self.dfs(t[char], board, y+1, x, path+char)
-            self.dfs(t[char], board, y, x-1, path+char)
-            self.dfs(t[char], board, y-1, x, path+char)
-            board[y][x] = char
-            if not t[char]:
-                t.pop(char)
+        if char not in node:
+            return
+        
+        next_node = node[char]
+        board[y][x] = '@'
+        dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
+        for d in range(4):
+            next_y, next_x = y + dy[d], x + dx[d]
+            self.dfs(board, next_node, next_y, next_x, word+char)
+        board[y][x] = char
+        if not next_node:
+            del node[char]
