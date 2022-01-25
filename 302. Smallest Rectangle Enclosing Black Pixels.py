5935bb02fe3b1a1b8cd005fefa7e99dad1653f64
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-01-25 13:59:56 +0900

    Create 302. Smallest Rectangle Enclosing Black Pixels.py

---
+++ 302. Smallest Rectangle Enclosing Black Pixels.py
@@ -0,0 +1,45 @@
+class Solution:
+    def minArea(self, image: List[List[str]], x: int, y: int) -> int:
+        m, n = len(image), len(image[0])
+        y, x = x, y
+        # search ymin
+        ymin = y
+        l, r = 0, y
+        while l <= r:
+            mid = (l + r) // 2
+            if '1' in image[mid]:
+                ymin = min(ymin, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+        # search ymax
+        ymax = y
+        l, r = y, m - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if '1' in image[mid]:
+                ymax = max(ymax, mid)
+                l = mid + 1
+            else:
+                r = mid - 1
+        # search xmin
+        xmin = x
+        l, r = 0, x
+        while l <= r:
+            mid = (l + r) // 2
+            if '1' in [image[i][mid] for i in range(m)]:
+                xmin = min(xmin, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+        # search xmax
+        xmax = x
+        l, r = x, n - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if '1' in [image[i][mid] for i in range(m)]:
+                xmax = max(xmax, mid)
+                l = mid + 1
+            else:
+                r = mid - 1
+        return (ymax - ymin + 1) * (xmax - xmin + 1)
