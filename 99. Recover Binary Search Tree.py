41700bbdd403c648b667dc987746e0946035e7fb
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-01-25 10:55:08 +0900

    Create 99. Recover Binary Search Tree.py

---
+++ 99. Recover Binary Search Tree.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        INF = pow(2, 31) + 1
+        # self.reg1: fisrt error value
+        # self.reg2: second error value
+        self.reg1, self.reg2 = -INF, INF
+        # found_flag: True if an error has been found
+        self.found_flag = False
+        # swap node1's & node2's value
+        self.node1, self.node2 = None, None
+        
+        self.dfs(root)
+        
+        temp = self.node1.val
+        self.node1.val = self.node2.val
+        self.node2.val = temp
+        
+        
+    def dfs(self, cur):
+        if cur.left != None:
+            self.dfs(cur.left)
+            
+        val = cur.val
+        # if no error until now
+        if not self.found_flag:
+            # valid
+            if val > self.reg1:
+                self.reg1 = val
+                self.node1 = cur
+            # error
+            else:
+                self.found_flag = True
+        if self.found_flag:
+            if val < self.reg1:
+                if val < self.reg2:
+                    self.reg2 = val
+                    self.node2 = cur
+            else:
+                return
+        
+        if cur.right != None:
+            self.dfs(cur.right)
+        
