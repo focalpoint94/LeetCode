c64f5b8036c442d1c343f8e24baa9ce169b773dd
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-01-29 14:40:34 +0900

    Create 126. Word Ladder II.py

---
+++ 126. Word Ladder II.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        wordSet = set(wordList)
+        wordSet.discard(beginWord)
+        if endWord not in wordSet:
+            return []
+        ret = []
+        q, finish = [[beginWord]], False
+        while q and not finish:
+            next_q = []
+            tobe_removed = set()
+            for elem in q:
+                prev = elem[-1]
+                for neighbor in self.get_neighbors(prev, wordSet):
+                    if neighbor == endWord:
+                        ret.append(elem+[endWord])
+                        finish = True
+                    else:
+                        next_q.append(elem+[neighbor])
+                        tobe_removed.add(neighbor)
+            wordSet -= tobe_removed
+            q = next_q
+        return ret
+                
+    def get_neighbors(self, word, wordSet):
+        neighbors = []
+        alphas = 'abcdefghijklmnopqrstuvwxyz'
+        for i in range(len(word)):
+            for j in range(len(alphas)):
+                candidate = word[:i] + alphas[j] + word[i+1:]
+                if candidate in wordSet:
+                    neighbors.append(candidate)
+        return neighbors
