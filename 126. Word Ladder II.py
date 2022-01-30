21e4c0363f89524728f55588027e2dbb6ac90c97
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-01-30 22:17:21 +0900

    Update 126. Word Ladder II.py

--- 126. Word Ladder II.py
+++ 126. Word Ladder II.py
@@ -1,5 +1,15 @@
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        def get_neighbors(word):
+            neighbors = []
+            alphas = 'abcdefghijklmnopqrstuvwxyz'
+            for i in range(len(word)):
+                for j in range(len(alphas)):
+                    candidate = word[:i] + alphas[j] + word[i+1:]
+                    if candidate in wordSet:
+                        neighbors.append(candidate)
+            return neighbors
+        
         wordSet = set(wordList)
         wordSet.discard(beginWord)
         if endWord not in wordSet:
@@ -11,7 +21,7 @@ class Solution:
             tobe_removed = set()
             for elem in q:
                 prev = elem[-1]
-                for neighbor in self.get_neighbors(prev, wordSet):
+                for neighbor in get_neighbors(prev):
                     if neighbor == endWord:
                         ret.append(elem+[endWord])
                         finish = True
@@ -21,13 +31,3 @@ class Solution:
             wordSet -= tobe_removed
             q = next_q
         return ret
-                
-    def get_neighbors(self, word, wordSet):
-        neighbors = []
-        alphas = 'abcdefghijklmnopqrstuvwxyz'
-        for i in range(len(word)):
-            for j in range(len(alphas)):
-                candidate = word[:i] + alphas[j] + word[i+1:]
-                if candidate in wordSet:
-                    neighbors.append(candidate)
-        return neighbors
