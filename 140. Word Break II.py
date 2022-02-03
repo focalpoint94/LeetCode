0401e9811e02053328100f9e99ec4cdf96b103a9
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-03 11:43:15 +0900

    Update 140. Word Break II.py

--- 140. Word Break II.py
+++ 140. Word Break II.py
@@ -1,14 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        self.wordDict = set(wordDict)
+        self.wordDict = wordDict
         self.ret = []
         self.dfs(s, [])
         return self.ret
-    
+        
     def dfs(self, s, path):
         if not s:
-            self.ret.append(" ".join(path))
+            self.ret.append(' '.join(path))
             return
-        for i in range(1, len(s)+1):
-            if s[:i] in self.wordDict:
-                self.dfs(s[i:], path+[s[:i]])
+        for word in self.wordDict:
+            k = len(word)
+            if s[:k] == word:
+                self.dfs(s[k:], path+[word])
