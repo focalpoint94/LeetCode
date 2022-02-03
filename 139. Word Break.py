838cd1234d10678535fb8199e87670999b202e82
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-03 11:44:59 +0900

    Update 139. Word Break.py

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -1,17 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        self.wordDict = set(wordDict)
-        self.dp = set()
+        self.wordDict = wordDict
+        self.visited = set()
         return self.dfs(s)
         
     def dfs(self, s):
         if not s:
             return True
-        if s in self.dp:
+        if s in self.visited:
             return False
-        for i in range(1, len(s)+1):
-            if s[:i] in self.wordDict:
-                if self.dfs(s[i:]):
+        for word in self.wordDict:
+            k = len(word)
+            if s[:k] == word:
+                if self.dfs(s[k:]):
                     return True
-        self.dp.add(s)
+        self.visited.add(s)
         return False
