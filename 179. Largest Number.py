475996398e0fcff10162ac546d362fba16abdb7e
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-07 10:48:34 +0900

    Update 179. Largest Number.py

--- 179. Largest Number.py
+++ 179. Largest Number.py
@@ -1,5 +1,7 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        nums = list(map(str, nums))
-        nums.sort(key=lambda x: x*10, reverse=True)
-        return str(int(''.join(nums)))
+        nums.sort(key=lambda x: str(x)*10, reverse=True)
+        ret = ''
+        for num in nums:
+            ret += str(num)
+        return ret.lstrip('0') if ret.lstrip('0') else '0'
