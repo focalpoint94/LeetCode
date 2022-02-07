74073cba86557386b7b09e98d669450a2d2851b9
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-07 10:17:14 +0900

    Create 169. Majority Element.py

---
+++ 169. Majority Element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count, candidate = 0, None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
