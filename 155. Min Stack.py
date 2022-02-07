7ecdb3a07d00826015cf3ccc277cc238f7abf50b
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-07 10:01:16 +0900

    Update 155. Min Stack.py

--- 155. Min Stack.py
+++ 155. Min Stack.py
@@ -1,31 +1,22 @@
-import heapq
 class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.h = []
-        self.p = []
-        
+
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        heapq.heappush(self.h, val)
+        if not self.stack:
+            self.stack.append([val, val])
+        else:
+            self.stack.append([val, min(val, self.stack[-1][1])])
 
     def pop(self) -> None:
-        val = self.stack.pop()
-        if val == self.h[0]:
-            heapq.heappop(self.h)
-            while self.h and self.p and self.h[0] == self.p[0]:
-                heapq.heappop(self.h)
-                heapq.heappop(self.p)
-        else:
-            heapq.heappush(self.p, val)
-        return val
-        
+        self.stack.pop()
+
     def top(self) -> int:
-        return self.stack[-1]
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.h[0]
+        return self.stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
