44a8d1dd5e45edb992d6c41982f2dcfefb2d15c0
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-12 20:04:49 +0900

    Update 334. Increasing Triplet Subsequence.py

--- 334. Increasing Triplet Subsequence.py
+++ 334. Increasing Triplet Subsequence.py
@@ -1,16 +1,16 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        dp = [nums[0]]
+        arr = [nums[0]]
         for i in range(1, len(nums)):
             num = nums[i]
             insert_flag = False
-            for j in range(len(dp)):
-                if num <= dp[j]:
-                    dp[j] = num
+            for j in range(len(arr)):
+                if num <= arr[j]:
+                    arr[j] = num
                     insert_flag = True
                     break
             if not insert_flag:
-                dp.append(num)
-            if len(dp) == 3:
+                arr.append(num)
+            if len(arr) == 3:
                 return True
         return False
