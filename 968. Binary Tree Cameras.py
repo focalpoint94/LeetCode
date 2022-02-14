a55d9dfbcb63508c7c1e959dd8ad7c44cb511e67
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-14 15:03:57 +0900

    Update 968. Binary Tree Cameras.py

--- 968. Binary Tree Cameras.py
+++ 968. Binary Tree Cameras.py
@@ -7,18 +7,35 @@
 class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
         self.ret = 0
-        root_state = self.dfs(root)
-        if root_state == 'should be covered':
-            return self.ret + 1
+        if self.dfs(root) == 'to be covered':
+            self.ret += 1
         return self.ret
         
     def dfs(self, node):
-        if not node:
-            return 'covered'
-        l, r = self.dfs(node.left), self.dfs(node.right)
-        if l == 'should be covered' or r == 'should be covered':
+        if not node.left and not node.right:
+            return 'to be covered'
+        if node.left and not node.right:
+            left = self.dfs(node.left)
+            if left == 'covered':
+                return 'to be covered'
+            if left == 'cam':
+                return 'covered'
+            if left == 'to be covered':
+                self.ret += 1
+                return 'cam'
+        if not node.left and node.right:
+            right = self.dfs(node.right)
+            if right == 'covered':
+                return 'to be covered'
+            if right == 'cam':
+                return 'covered'
+            if right == 'to be covered':
+                self.ret += 1
+                return 'cam'
+        left, right = self.dfs(node.left), self.dfs(node.right)
+        if left == 'to be covered' or right == 'to be covered':
             self.ret += 1
-            return 'covering'
-        if l == 'covering' or r == 'covering':
+            return 'cam'
+        if left == 'cam' or right == 'cam':
             return 'covered'
-        return 'should be covered'
+        return 'to be covered'
