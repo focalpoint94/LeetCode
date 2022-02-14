2320d5becb5598c7d727182ce2ff122db719da79
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-14 15:19:59 +0900

    Update 1011. Capacity To Ship Packages Within D Days.py

--- 1011. Capacity To Ship Packages Within D Days.py
+++ 1011. Capacity To Ship Packages Within D Days.py
@@ -1,29 +1,23 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        max_capacity = 0
-        for w in weights:
-            max_capacity += w
+        def count(val):
+            cnt = 0
+            temp = 0
+            for weight in weights:
+                if temp + weight <= val:
+                    temp += weight
+                else:
+                    cnt += 1
+                    temp = weight
+            return cnt + 1
         
-        ret = max_capacity
-        l, r = 0, max_capacity
-        while l <= r:
-            capacity = (l + r) // 2
-            if self.isEnoughCapacity(weights, capacity, days):
-                ret = min(ret, capacity)
-                r = capacity - 1
+        ret = sum(weights)
+        left, right = max(weights), sum(weights)
+        while left <= right:
+            mid = (left + right) // 2
+            if count(mid) <= days:
+                ret = min(ret, mid)
+                right = mid - 1
             else:
-                l = capacity + 1
+                left = mid + 1
         return ret
-    
-    
-    def isEnoughCapacity(self, weights, capacity, days):
-        if capacity < max(weights):
-            return False
-        num_days = 1
-        tempsum = 0
-        for w in weights:
-            tempsum += w
-            if tempsum > capacity:
-                num_days += 1
-                tempsum = w
-        return (num_days <= days)
