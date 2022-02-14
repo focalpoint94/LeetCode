955769c14243725b5a9669d2012bf58305c4f79f
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-14 10:51:50 +0900

    Update 371. Sum of Two Integers.py

--- 371. Sum of Two Integers.py
+++ 371. Sum of Two Integers.py
@@ -1,18 +1,17 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        # if b is 0, it doesn't enter while loop
-        # in that case, code after while loop doesn't make sense
-        if b == 0:
-            return a
-        
-        # bit_mask (32 ones; 1111111..111) is needed to deal with the last 32 bits
-        # as python uses infinite number of bits to represent integers
-        bit_mask = 0xffffffff
-        while b:
-            summation = (a ^ b) & bit_mask
-            carry = ((a & b) << 1) & bit_mask
-            a, b = summation, carry
-        
-        if (a >> 31) & 1:
-            return ~ (a ^ bit_mask)
-        return a
+        x, y = abs(a), abs(b)
+        if x < y:
+            return self.getSum(b, a)
+        sign = 1 if a > 0 else -1
+        if a * b >= 0:
+            while y:
+                summed = x ^ y
+                carry = (x & y) << 1
+                x, y = summed, carry
+        else:
+            while y:
+                summed = x ^ y
+                borrow = ((~x) & y) << 1
+                x, y = summed, borrow
+        return x * sign
