42bd189e808b200bad260e16716e970ed00a9e04
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-02-15 11:14:30 +0900

    Update 132. Palindrome Partitioning II.py

--- 132. Palindrome Partitioning II.py
+++ 132. Palindrome Partitioning II.py
@@ -1,22 +1,23 @@
 class Solution:
     def minCut(self, s: str) -> int:
-        dp = self.build_palindrome(s)
-        n = len(s)
-        self.cut = [2002] * n
-        for end in range(n):
-            if dp[0][end]:
-                self.cut[end] = 0
+        palin = self.palindrome(s)
+        dp = [2002] * len(s)
+        dp[0] = 0
+        for i in range(1, len(s)):
+            if palin[0][i]:
+                dp[i] = 0
                 continue
-            for start in range(1, end+1):
-                if dp[start][end]:
-                    self.cut[end] = min(self.cut[end], self.cut[start-1] + 1)
-        return self.cut[-1]
-        
-    def build_palindrome(self, word):
+            for j in range(1, i+1):
+                if palin[j][i]:
+                    dp[i] = min(dp[i], dp[j-1]+1)
+        return dp[-1]
+    
+    def palindrome(self, word):
         n = len(word)
-        dp = [[False] * n for _ in range(n)]
-        for end in range(n):
-            for start in range(end+1):
-                if word[end] == word[start] and (end - start <= 2 or dp[start+1][end-1]):
-                    dp[start][end] = True
-        return dp
+        table = [[False] * n for _ in range(n)]
+        for j in range(n):
+            for i in range(j+1):
+                if word[j] == word[i] and (j - i <= 2 or table[i+1][j-1]):
+                    table[i][j] = True
+        return table
+        
