937497f7c7c68640376fd3dd901dae191d5015eb
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-03-02 13:22:26 +0900

    Update 57. Insert Interval.py

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -1,21 +1,11 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        ret = []
-        i = 0
-        for i, interval in enumerate(intervals):
-            if interval[0] <= newInterval[0]:
-                ret.append(interval)
-            else:
-                break
-        
-        if ret and ret[-1][1] >= newInterval[0]:
-            ret[-1][1] = max(ret[-1][1], newInterval[1])
-        else:
-            ret.append(newInterval)
-            
-        for j in range(i, len(intervals)):
-            interval = intervals[j]
-            if ret and ret[-1][1] >= interval[0]:
+        from bisect import insort
+        idx = insort(intervals, newInterval)
+        ret = [intervals[0]]
+        for i in range(1, len(intervals)):
+            interval = intervals[i]
+            if ret[-1][1] >= interval[0]:
                 ret[-1][1] = max(ret[-1][1], interval[1])
             else:
                 ret.append(interval)
