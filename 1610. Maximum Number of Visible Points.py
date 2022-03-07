42c6e6d62bb4ac661c2ed854f03a51c8fd25fef4
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-03-07 14:32:22 +0900

    Create 1610. Maximum Number of Visible Points.py

---
+++ 1610. Maximum Number of Visible Points.py
@@ -0,0 +1,23 @@
+class Solution:
+    def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:   
+        from math import atan2, pi
+        from bisect import bisect_right
+        numLoc = 0
+        angles = []
+        for point in points:
+            x, y = point[0] - location[0], point[1] - location[1]
+            if x == 0 and y == 0:
+                numLoc += 1
+            else:
+                val = atan2(y, x) * 180 / pi
+                if val < 0:
+                    val += 360
+                angles.append(val)
+        angles.sort()
+        angles += [angle+360 for angle in angles]
+        
+        ret = numLoc
+        for i in range(len(angles)//2):
+            j = bisect_right(angles, angles[i]+angle)
+            ret = max(ret, j - i + numLoc)
+        return ret
