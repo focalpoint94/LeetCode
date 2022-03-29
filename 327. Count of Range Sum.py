36c5f0ff8e73a8b4c9e158bd28580c02a50ba81b
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-03-29 10:31:18 +0900

    Update 327. Count of Range Sum.py

--- 327. Count of Range Sum.py
+++ 327. Count of Range Sum.py
@@ -1,20 +1,27 @@
 class Solution:
-    def countRangeSum(self, nums, lower, upper):
-        sums = [0] * (len(nums) + 1)
-        for i, num in enumerate(nums):
-            sums[i+1] = sums[i] + num
-        def ms(left, right):
-            if left == right:
-                return 0
-            mid = (left + right) // 2
-            count = ms(left, mid) + ms(mid+1, right)
-            i, j = mid+1, mid+1
-            for s in sums[left:mid+1]:
-                while i <= right and sums[i] - s < lower:
-                    i += 1
-                while j <= right and sums[j] - s <= upper:
-                    j += 1
-                count += j - i
-            sums[left:right+1] = sorted(sums[left:right+1])
-            return count
-        return ms(0, len(sums)-1)
+    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+        self.lower, self.upper = lower, upper
+        n = len(nums)
+        sums, prev = [0] * (n+1), 0
+        for i in range(n):
+            sums[i+1] = nums[i] + prev
+            prev += nums[i]
+        self.ret = 0
+        self.mergeSort(sums, 0, n)
+        return self.ret
+        
+    def mergeSort(self, sums, left, right):
+        if left == right:
+            return [sums[left]]
+        mid = (left + right) // 2
+        lsums = self.mergeSort(sums, left, mid)
+        rsums = self.mergeSort(sums, mid+1, right)
+        l, r = 0, 0
+        for lval in lsums:
+            while r < len(rsums) and rsums[r] - lval <= self.upper:
+                r += 1
+            while l < len(rsums) and rsums[l] - lval < self.lower:
+                l += 1
+            self.ret += r - l
+        sums[left:right+1] = sorted(sums[left:right+1])
+        return sums[left:right+1]
