b6d000e4dc128e604cc17dcfb61659367c493488
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-03-30 10:12:45 +0900

    Update 4. Median of Two Sorted Arrays.py

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -1,28 +1,23 @@
-INF = int(1e9)
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
         m, n = len(nums1), len(nums2)
-        if m > n:
-            nums1, nums2, m, n = nums2, nums1, n, m
-        
-        left, right, p = 0, m, (m+n+1) // 2
+        left, right = 0, m
         while left <= right:
             p1 = (left + right) // 2
-            p2 = p - p1
+            p2 = (m + n) // 2 - p1
             
-            maxleft1 = -INF if p1 == 0 else nums1[p1-1]
-            minright1 = INF if p1 == m else nums1[p1]
+            l1 = nums1[p1-1] if p1 != 0 else -float('inf')
+            r1 = nums1[p1] if p1 != m else float('inf')
+            l2 = nums2[p2-1] if p2 != 0 else -float('inf')
+            r2 = nums2[p2] if p2 != n else float('inf')
             
-            maxleft2 = -INF if p2 == 0 else nums2[p2-1]
-            minright2 = INF if p2 == n else nums2[p2]
-            
-            if maxleft1 <= minright2 and maxleft2 <= minright1:
-                if (m + n) % 2 == 0:
-                    return (max(maxleft1, maxleft2) + min(minright1, minright2))/2.
-                return max(maxleft1, maxleft2)
-            
-            elif maxleft1 > minright2:
+            if l1 > r2:
                 right = p1 - 1
-            
-            else:
+            elif l2 > r1:
                 left = p1 + 1
+            else:
+                if (m + n) % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2)) / 2
+                return min(r1, r2)
