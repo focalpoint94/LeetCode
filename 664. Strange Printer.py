1658af8cf5b5b040b7e384747a21e1bc7074e2de
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-05 10:17:48 +0900

    Update 664. Strange Printer.py

--- 664. Strange Printer.py
+++ 664. Strange Printer.py
@@ -1,19 +1,15 @@
 class Solution:
     def strangePrinter(self, s: str) -> int:
-        S = ''.join(a for a, b in zip(s, '#' + s) if a != b)
-        n = len(s)
-        dp = [[n] * n for _ in range(n)]
-        for i in range(n):
+        N = len(s)
+        dp = [[N] * N for _ in range(N)]
+        for i in range(N):
             dp[i][i] = 1
-        for length in range(2, n+1):
-            for i in range(n):
-                j = i + length - 1
-                if j >= n:
-                    break
-                if s[i] == s[j]:
-                    for k in range(j):
-                        dp[i][j] = min(dp[i][j], dp[i][k]+dp[k+1][j]-1)
-                else:
-                    for k in range(j):
-                        dp[i][j] = min(dp[i][j], dp[i][k]+dp[k+1][j])
+        for length in range(2, len(s)+1):
+            for i in range(len(s)-length+1):
+                end = i + length - 1
+                for j in range(i+1, end+1):
+                    if s[i] == s[end]:
+                        dp[i][end] = min(dp[i][end], dp[i][j-1] + dp[j][end] - 1)
+                    else:
+                        dp[i][end] = min(dp[i][end], dp[i][j-1] + dp[j][end])
         return dp[0][-1]
