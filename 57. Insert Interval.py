c547e24017867942c59882888bc85053d5d644f1
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-06 10:20:58 +0900

    Update 57. Insert Interval.py

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -10,3 +10,28 @@ class Solution:
             else:
                 ret.append(interval)
         return ret
+
+# class Solution:
+#     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+#         newIntervals = []
+#         i = 0
+#         for i in range(len(intervals)):
+#             interval = intervals[i]
+#             if interval[0] <= newInterval[0]:
+#                 newIntervals.append(interval)
+#             else:
+#                 break
+        
+#         if newIntervals and newInterval[0] <= newIntervals[-1][1]:
+#             newIntervals[-1][1] = max(newIntervals[-1][1], newInterval[1])
+#         else:
+#             newIntervals.append(newInterval)
+        
+#         for j in range(i, len(intervals)):
+#             interval = intervals[j]
+#             if interval[0] <= newIntervals[-1][1]:
+#                 newIntervals[-1][1] = max(newIntervals[-1][1], interval[1])
+#             else:
+#                 newIntervals.append(interval)
+        
+#         return newIntervals
