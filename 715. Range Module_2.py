bb4841c4465f598cee69d59ce253d1444d9ce2d0
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-07 09:24:57 +0900

    Update 715. Range Module_2.py

--- 715. Range Module_2.py
+++ 715. Range Module_2.py
@@ -1,36 +1,33 @@
+from bisect import bisect_left, bisect_right
 class RangeModule:
-    from bisect import bisect_left, bisect_right
-    
+
     def __init__(self):
         self.range = []
-    
+        
     def addRange(self, left: int, right: int) -> None:
         start = bisect_left(self.range, left)
         end = bisect_right(self.range, right)
-        
-        new_range = []
+        newRange = []
         if start % 2 == 0:
-            new_range.append(left)
+            newRange.append(left)
         if end % 2 == 0:
-            new_range.append(right)
-        self.range[start:end] = new_range
+            newRange.append(right)
+        self.range[start:end] = newRange
         
     def queryRange(self, left: int, right: int) -> bool:
         start = bisect_right(self.range, left)
         end = bisect_left(self.range, right)
         return start == end and start % 2 == 1
-    
+
     def removeRange(self, left: int, right: int) -> None:
         start = bisect_left(self.range, left)
         end = bisect_right(self.range, right)
-        
-        new_range = []
+        newRange = []
         if start % 2 == 1:
-            new_range.append(left)
+            newRange.append(left)
         if end % 2 == 1:
-            new_range.append(right)
-        self.range[start:end] = new_range
-
+            newRange.append(right)
+        self.range[start:end] = newRange
 
 # Your RangeModule object will be instantiated and called as such:
 # obj = RangeModule()
