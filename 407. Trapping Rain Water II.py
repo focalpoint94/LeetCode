13d50a3d39d88b6d5e587a2e5d682c7ffe84dc61
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-08 14:19:28 +0900

    Update 407. Trapping Rain Water II.py

--- 407. Trapping Rain Water II.py
+++ 407. Trapping Rain Water II.py
@@ -1,27 +1,38 @@
+import heapq
+
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
-        import heapq
-        boundaries = []
         m, n = len(heightMap), len(heightMap[0])
-        
-        visited = [[False] * n for _ in range(m)]
-        for y in range(m):
-            for x in range(n):
-                if y == 0 or y == m - 1 or x == 0 or x == n - 1:
-                    heapq.heappush(boundaries, (heightMap[y][x], y, x))
-                    visited[y][x] = True
-        
-        ret = 0
+        boundaries = []
+        for i in range(n):
+            boundaries.append((0, i))
+            boundaries.append((m - 1, i))
+        for i in range(1, m - 1):
+            boundaries.append((i, 0))
+            boundaries.append((i, n - 1))
+
+        # heights: min heap of boundaries
+        # if popped, will return min height of the current boundaries
+        heights = []
+        for y, x in boundaries:
+            heapq.heappush(heights, (heightMap[y][x], y, x))
+
+        visited = set()
+        visited.update(boundaries)
+
         dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
-        while boundaries:
-            h, y, x = heapq.heappop(boundaries)    
+
+        ret = 0
+
+        while heights:
+            h, y, x = heapq.heappop(heights)
             for d in range(4):
                 next_y, next_x = y + dy[d], x + dx[d]
-                if 0 <= next_y < m and 0 <= next_x < n and not visited[next_y][next_x]:
-                    if heightMap[next_y][next_x] >= h:
-                        heapq.heappush(boundaries, (heightMap[next_y][next_x], next_y, next_x))
-                    else:
+                if 0 <= next_y < m and 0 <= next_x < n and (next_y, next_x) not in visited:
+                    visited.add((next_y, next_x))
+                    if heightMap[next_y][next_x] <= h:
                         ret += h - heightMap[next_y][next_x]
-                        heapq.heappush(boundaries, (h, next_y, next_x))
-                    visited[next_y][next_x] = True            
-        return ret
+                        heapq.heappush(heights, (h, next_y, next_x))
+                    else:
+                        heapq.heappush(heights, (heightMap[next_y][next_x], next_y, next_x))
+        return ret    
