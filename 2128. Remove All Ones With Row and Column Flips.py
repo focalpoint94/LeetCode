d24582f0041cfa9639e9d36538147a988b3e03c4
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-11 09:35:13 +0900

    Update 2128. Remove All Ones With Row and Column Flips.py

--- 2128. Remove All Ones With Row and Column Flips.py
+++ 2128. Remove All Ones With Row and Column Flips.py
@@ -1,8 +1,7 @@
 class Solution:
     def removeOnes(self, grid: List[List[int]]) -> bool:
-        ref1 = grid[0]
-        ref2 = [x^1 for x in grid[0]]
-        for row in grid:
-            if row != ref1 and row != ref2:
+        samples = [grid[0], [x^1 for x in grid[0]]]
+        for i in range(1, len(grid)):
+            if grid[i] not in samples:
                 return False
-        return True
+        return True        
