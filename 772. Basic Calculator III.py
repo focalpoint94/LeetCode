f44760ab56f98633c9edf8de8769fdf36cba6278
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-12 10:49:34 +0900

    Update 772. Basic Calculator III.py

--- 772. Basic Calculator III.py
+++ 772. Basic Calculator III.py
@@ -1,49 +1,42 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # ' ' is intentionally added,
-        # to mark the end of the given string s
-        # encountering ' ' will add the last number to the stack
         self.s = s + ' '
         self.ptr = 0
-        return self.calc()
-        
-    def calc(self):
-        num, stack, sign = 0, [], '+'
+        return self.helper()
+    
+    def helper(self):
+        stack, num, sign = [], 0, '+'
         while self.ptr < len(self.s):
-            c = self.s[self.ptr]
-            
-            # if met with '(',
-            # get the evaluated number by recursion
-            if c == '(':
+            char = self.s[self.ptr]
+            if char.isdigit():
+                num = num * 10 + int(char)
                 self.ptr += 1
-                num = self.calc()
-            
-            # deal with digits
-            elif c.isdigit():
-                num = num * 10 + int(c)
-                self.ptr += 1
-            
-            # if met with operators or ')' or ' ',
-            else:
-                # if met with operators,
-                # stack the number and update sign & num
+            elif char in ['+', '-', '*', '/', ' ']:
                 if sign == '+':
                     stack.append(num)
                 elif sign == '-':
                     stack.append(-num)
                 elif sign == '*':
                     stack.append(stack.pop()*num)
-                elif sign == '/':
+                else:
                     stack.append(int(stack.pop()/num))
-                
-                # if met with ')',
-                # stack the number and return sum(stack)
-                if c == ')':
-                    self.ptr += 1
-                    return sum(stack)
-                
-                sign = c
+                sign = char
                 num = 0
                 self.ptr += 1
-        
+            elif char == ')':
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack.append(stack.pop()*num)
+                else:
+                    stack.append(int(stack.pop()/num))
+                self.ptr += 1
+                return sum(stack)
+            else:
+                self.ptr += 1
+                num = self.helper()
         return sum(stack)
+        
+        
