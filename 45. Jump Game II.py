6001c888eb274015ac67733d850192451dcf77cb
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-15 10:27:36 +0900

    Update 45. Jump Game II.py

--- 45. Jump Game II.py
+++ 45. Jump Game II.py
@@ -1,15 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        if len(nums) <= 1:
             return 0
-        now, jump = 0, 0
+        numJump, pos = 0, 0
         while True:
-            if now + nums[now] >= len(nums) - 1:
-                return jump + 1
-            val, next_idx = -1, -1
-            for i in range(now+1, min(now+nums[now]+1, len(nums))):
-                if i + nums[i] > val:
-                    val = i + nums[i]
-                    next_idx = i
-            now = next_idx
-            jump += 1
+            if pos + nums[pos] >= len(nums) - 1:
+                return numJump + 1
+            nextPos, value = -1, -1
+            for candidate in range(pos+1, min(pos+nums[pos]+1, len(nums))):
+                if candidate + nums[candidate] > value:
+                    nextPos, value = candidate, candidate + nums[candidate]
+            numJump += 1
+            pos = nextPos
