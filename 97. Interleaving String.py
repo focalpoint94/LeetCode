eec9cf1a052e11954928d7717c64b20db4ef51af
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-19 16:02:34 +0900

    Update 97. Interleaving String.py

--- 97. Interleaving String.py
+++ 97. Interleaving String.py
@@ -1,23 +1,22 @@
+from functools import lru_cache
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
             return False
-        self.s1 = s1
-        self.s2 = s2
-        self.s3 = s3
-        self.dp = set()
-        return self.dfs(0, 0, 0)
+        self.s1, self.s2, self.s3 = s1, s2, s3
+        return self.backTrack(0, 0)
         
-    def dfs(self, i, j, k):
-        if (i, j, k) in self.dp:
-            return False
-        if k == len(self.s3):
+    @lru_cache()
+    def backTrack(self, p1, p2):
+        if p1 + p2 == len(self.s3):
             return True
-        if i < len(self.s1) and self.s1[i] == self.s3[k]:
-            if self.dfs(i+1, j, k+1):
+        
+        if p1 < len(self.s1) and self.s3[p1+p2] == self.s1[p1]:
+            if self.backTrack(p1+1, p2):
                 return True
-        if j < len(self.s2) and self.s2[j] == self.s3[k]:
-            if self.dfs(i, j+1, k+1):
+        
+        if p2 < len(self.s2) and self.s3[p1+p2] == self.s2[p2]:
+            if self.backTrack(p1, p2+1):
                 return True
-        self.dp.add((i, j, k))
+            
         return False
