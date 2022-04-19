b43e1543762ca2d4ee382d83c375baa4af9daee8
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-19 15:50:55 +0900

    Update 91. Decode Ways.py

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -1,19 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        candidates = set([str(i) for i in range(1, 27)])
-        # dp[i] denotes # of ways until s[i-1]
-        dp = [0] * (len(s) + 1)
-        dp[0] = 1
-        if s[0] in candidates:
-            dp[1] = 1
+        if s[0] == '0':
+            return 0
         
-        for i in range(2, len(s)+1):
-            # dp[n] = dp[n-2] * k2 + dp[n-1] * k1
-            k2 = 0
-            if s[i-2:i] in candidates:
-                k2 += 1
-            k1 = 0
-            if s[i-1] in candidates:
-                k1 += 1
-            dp[i] = dp[i-2] * k2 + dp[i-1] * k1
-        return dp[len(s)]
+        decoder = {str(i): chr(i+64) for i in range(1, 27)}
+        N = len(s)
+        dp = [0] * (N+1)
+        dp[0] = dp[1] = 1
+        
+        for i in range(2, N+1):
+            if s[i-1] in decoder:
+                dp[i] += dp[i-1]
+            if s[i-2:i] in decoder:
+                dp[i] += dp[i-2]
+        return dp[-1]
