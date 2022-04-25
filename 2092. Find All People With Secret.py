dc83b8d7381a09eeee4398e7ebb3fa75def529fe
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-25 12:11:10 +0900

    Update 2092. Find All People With Secret.py

--- 2092. Find All People With Secret.py
+++ 2092. Find All People With Secret.py
@@ -1,6 +1,6 @@
 
 '''
-BFS Solution
+BFS Solution - 1
 '''
 
 from collections import defaultdict, deque
@@ -30,4 +30,34 @@ class Solution:
             graph[y].add(x)
             if x in knowing: processing.add(x)
             if y in knowing: processing.add(y)
+        return knowing
+
+    
+    
+'''
+BFS Solution - 2
+'''
+
+from collections import defaultdict, deque
+from itertools import groupby
+
+class Solution:
+    def findAllPeople(self, n: int, meetings, firstPerson: int):
+        knowing = set([0, firstPerson])
+        for _, group in groupby(sorted(meetings, key=lambda x: x[2]), key=lambda x: x[2]):
+            q = set()
+            graph = defaultdict(set)
+            for x, y, t in group:
+                graph[x].add(y)
+                graph[y].add(x)
+                if x in knowing: q.add(x)
+                if y in knowing: q.add(y)
+            
+            q = deque(q)
+            while q:
+                u = q.popleft()
+                for v in graph[u]:
+                    if v not in knowing:
+                        knowing.add(v)
+                        q.append(v)
         return knowing
