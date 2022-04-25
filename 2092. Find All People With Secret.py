4f2254ffdc6dfccce8943bb12b2a451599e34bc2
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-25 12:01:59 +0900

    Update 2092. Find All People With Secret.py

--- 2092. Find All People With Secret.py
+++ 2092. Find All People With Secret.py
@@ -1,30 +1,33 @@
 
 '''
-TLE
+BFS Solution
 '''
 
-from collections import defaultdict
+from collections import defaultdict, deque
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
         meetings.sort(key=lambda x: x[2])
         meetings.append([0, firstPerson, 10**5+1])
         
         knowing, graph = set([0, firstPerson]), defaultdict(set)
+        processing = set()
         prevTime = 0
         
         for x, y, t in meetings:
             # new time
             if t != prevTime:
-                processing, processed = list(knowing), set(knowing)
+                processing = deque(processing)
                 while processing:
-                    u = processing.pop()
-                    processed.add(u)
+                    u = processing.popleft()
                     for v in graph[u]:
-                        knowing.add(v)
-                        if v not in processed:
+                        if v not in knowing:
+                            knowing.add(v)
                             processing.append(v)
                 graph = defaultdict(set)
+                processing = set()
                 prevTime = t
             graph[x].add(y)
             graph[y].add(x)
-        return list(knowing)
+            if x in knowing: processing.add(x)
+            if y in knowing: processing.add(y)
+        return knowing
