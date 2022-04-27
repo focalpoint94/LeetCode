87a6388cf7bf2c857067b355a699075971a42a73
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-04-27 09:25:27 +0900

    Update 188. Best Time to Buy and Sell Stock IV.py

--- 188. Best Time to Buy and Sell Stock IV.py
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -3,12 +3,10 @@ class Solution:
         if not prices:
             return 0
         n = len(prices)
-        dp = [[0] * (n) for _ in range(k+1)]
-        ret = 0
-        for i in range(1, k+1):
-            prevprofit = -prices[0]
-            for j in range(1, n):
-                prevprofit = max(prevprofit, dp[i-1][j] - prices[j])
-                dp[i][j] = max(dp[i][j-1], prevprofit + prices[j])
-                ret = max(ret, dp[i][j])
-        return ret
+        profit = [[0] * n for _ in range(k+1)]
+        for t in range(1, k+1):
+            maxJ = profit[t-1][0] - prices[0]
+            for i in range(n):
+                maxJ = max(maxJ, profit[t-1][i] - prices[i])
+                profit[t][i] = max(profit[t][i-1], prices[i] + maxJ)
+        return profit[-1][-1]
