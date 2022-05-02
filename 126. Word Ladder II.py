d9e47fc95ac47ac770f8aed56063ff4b2efc5061
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-05-02 11:02:09 +0900

    Update 126. Word Ladder II.py

--- 126. Word Ladder II.py
+++ 126. Word Ladder II.py
@@ -1,33 +1,29 @@
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        def get_neighbors(word):
-            neighbors = []
-            alphas = 'abcdefghijklmnopqrstuvwxyz'
-            for i in range(len(word)):
-                for j in range(len(alphas)):
-                    candidate = word[:i] + alphas[j] + word[i+1:]
-                    if candidate in wordSet:
-                        neighbors.append(candidate)
-            return neighbors
+        self.wordSet = set(wordList)
+        self.wordSet.discard(beginWord)
+        if endWord not in self.wordSet: return []
         
-        wordSet = set(wordList)
-        wordSet.discard(beginWord)
-        if endWord not in wordSet:
-            return []
-        ret = []
-        q, finish = [[beginWord]], False
-        while q and not finish:
-            next_q = []
-            tobe_removed = set()
-            for elem in q:
-                prev = elem[-1]
-                for neighbor in get_neighbors(prev):
-                    if neighbor == endWord:
-                        ret.append(elem+[endWord])
-                        finish = True
-                    else:
-                        next_q.append(elem+[neighbor])
-                        tobe_removed.add(neighbor)
-            wordSet -= tobe_removed
-            q = next_q
-        return ret
+        paths, finished = [[beginWord]], False
+        while not finished:
+            if not paths: return []
+            next_paths, visited = [], set()
+            while paths:
+                path = paths.pop()
+                for adjacent in self.adjacents(path[-1]):
+                    if adjacent == endWord:
+                        finished = True
+                    next_paths.append(path+[adjacent])
+                    visited.add(adjacent)
+            self.wordSet -= visited
+            paths = next_paths
+        return [path for path in paths if path[-1] == endWord]        
+        
+    
+    def adjacents(self, word):
+        alphas = 'abcdefghijklmnopqrstuvwxyz'
+        for i in range(len(word)):
+            for alpha in alphas:
+                candidate = word[:i] + alpha + word[i+1:]
+                if candidate in self.wordSet:
+                    yield candidate
