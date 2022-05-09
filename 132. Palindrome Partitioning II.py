fa110eea837622b8ee064db177897f772be5c0ec
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-05-09 09:29:47 +0900

    Update 132. Palindrome Partitioning II.py

--- 132. Palindrome Partitioning II.py
+++ 132. Palindrome Partitioning II.py
@@ -1,23 +1,30 @@
 class Solution:
     def minCut(self, s: str) -> int:
+        N = len(s)
         palin = self.palindrome(s)
-        dp = [2002] * len(s)
+        dp = [N] * N
         dp[0] = 0
-        for i in range(1, len(s)):
+        for i in range(1, N):
             if palin[0][i]:
                 dp[i] = 0
-                continue
-            for j in range(1, i+1):
-                if palin[j][i]:
-                    dp[i] = min(dp[i], dp[j-1]+1)
+            else:
+                dp[i] = dp[i-1] + 1
+                for j in range(i):
+                    if palin[j+1][i]:
+                        dp[i] = min(dp[i], dp[j]+1)
         return dp[-1]
-    
-    def palindrome(self, word):
-        n = len(word)
-        table = [[False] * n for _ in range(n)]
-        for j in range(n):
-            for i in range(j+1):
-                if word[j] == word[i] and (j - i <= 2 or table[i+1][j-1]):
-                    table[i][j] = True
-        return table
         
+    def palindrome(self, s):
+        N = len(s)
+        isPalin = [[False] * N for _ in range(N)]
+        for i in range(N):
+            isPalin[i][i] = True
+        for i in range(N-1):
+            if s[i] == s[i+1]:
+                isPalin[i][i+1] = True
+        for length in range(3, N+1):
+            for i in range(N):
+                j = i + length - 1
+                if j <= len(s) - 1 and isPalin[i+1][j-1] and s[i] == s[j]:
+                    isPalin[i][j] = True
+        return isPalin
