749097beb320cd01620803f621f6ebe6fd06fbba
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-05-10 11:13:13 +0900

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -1,6 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        num = nums[0]
-        for i in range(1, len(nums)):
-            num ^= nums[i]
-        return num
+        from functools import reduce
+        from operator import xor
+        return reduce(xor, nums)
+#         num = nums[0]
+#         for i in range(1, len(nums)):
+#             num ^= nums[i]
+#         return num
