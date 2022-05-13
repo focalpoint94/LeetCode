6f3582587ab0bf633f013fe244e3e784fe1f4be2
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-05-13 13:22:13 +0900

    Update 146. LRU Cache.py

--- 146. LRU Cache.py
+++ 146. LRU Cache.py
@@ -1,27 +1,32 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = dict()
+        self.dict = {}
         self.capacity = capacity
-        self.cnt = 0
-        
+
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.cache[key] = self.cache.pop(key)
-            return self.cache[key]
-        return -1
+        if key in self.dict:
+            ret = self.dict.pop(key)
+            self.dict[key] = ret
+            return ret
+        else:
+            return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.pop(key)
-            self.cache[key] = value
+        if key in self.dict:
+            self.dict.pop(key)
+            self.dict[key] = value
         else:
-            if self.cnt == self.capacity:
-                self.cache.pop(next(iter(self.cache)))
-                self.cnt -= 1
-            self.cache[key] = value
-            self.cnt += 1
+            if len(self.dict) == self.capacity:
+                self.dict.pop(next(iter(self.dict)))
+            self.dict[key] = value
+        
 
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
 # class DLinkedNode():
 #     def __init__(self, key=None, val=None, prv=None, nxt=None):
 #         self.key = key
