9159eb63bc7a8682220ece4e2f3e492ad1aa355d
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-05-16 11:55:30 +0900

    Update 152. Maximum Product Subarray.py

--- 152. Maximum Product Subarray.py
+++ 152. Maximum Product Subarray.py
@@ -3,17 +3,14 @@ class Solution:
         ret = -float('inf')
         prod = 1
         for num in nums:
-            ret = max(ret, num)
-            if num == 0:
-                prod = 1
-                continue
             prod *= num
             ret = max(ret, prod)
+            if prod == 0:
+                prod = 1
         prod = 1
         for num in nums[::-1]:
-            if num == 0:
-                prod = 1
-                continue
             prod *= num
             ret = max(ret, prod)
+            if prod == 0:
+                prod = 1
         return ret
