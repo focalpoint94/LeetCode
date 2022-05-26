c583fd35c81c5d36139bf91b4b74c74bada4fd01
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-05-26 11:52:51 +0900

    Update 200. Number of Islands.py

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -1,23 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        self.grid = grid
-        self.m, self.n = len(self.grid), len(self.grid[0])
-        self.visited = [[False] * self.n for _ in range(self.m)]
-        dy = [0, 1, 0, -1]
-        dx = [1, 0, -1, 0]
-        ret = 0
-        for i in range(self.m):
-            for j in range(self.n):
-                if self.grid[i][j] == '1' and not self.visited[i][j]:
-                    self.dfs(i, j, dy, dx)
-                    ret += 1
-        return ret  
+        m, n = len(grid), len(grid[0])
+        dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
         
-    def dfs(self, y, x, dy, dx):
-        self.visited[y][x] = True
-        for t in range(4):
-            next_y = y + dy[t]
-            next_x = x + dx[t]
-            if 0 <= next_y <= self.m - 1 and 0 <= next_x <= self.n - 1:
-                if self.grid[next_y][next_x] == '1' and not self.visited[next_y][next_x]:
-                    self.dfs(next_y, next_x, dy, dx)
+        visited = set()
+        
+        def dfs(y, x):
+            grid[y][x] = '2'
+            visited.add((y, x))
+            for d in range(4):
+                next_y, next_x = y + dy[d], x + dx[d]
+                if 0 <= next_y < m and 0 <= next_x < n:
+                    if grid[next_y][next_x] == '1':
+                        if (next_y, next_x) not in visited:
+                            dfs(next_y, next_x)
+        
+        ret = 0
+        for y in range(m):
+            for x in range(n):
+                if grid[y][x] == '1':
+                    dfs(y, x)
+                    ret += 1
+        return ret
