1ca47a61df2ffa895fec3287639e182ac1fe5f30
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-05-26 11:38:11 +0900

    Update 189. Rotate Array Medium.py

--- 189. Rotate Array Medium.py
+++ 189. Rotate Array Medium.py
@@ -6,16 +6,15 @@ class Solution:
         if k == 0:
             return
         k %= len(nums)
-        self.inplace_reverse(nums, 0, len(nums)-1-k)
-        self.inplace_reverse(nums, len(nums)-k, len(nums)-1)
-        self.inplace_reverse(nums, 0, len(nums)-1)        
         
-    def inplace_reverse(self, arr, l, r):
-        if not arr:
-            return
-        while l < r:
-            temp = arr[l]
-            arr[l] = arr[r]
-            arr[r] = temp
-            l += 1
-            r -= 1
+        def inplace_reverse(l, r):
+            while l < r:
+                temp = nums[l]
+                nums[l] = nums[r]
+                nums[r] = temp
+                l += 1
+                r -= 1
+        
+        inplace_reverse(0, len(nums)-1-k)
+        inplace_reverse(len(nums)-k, len(nums)-1)
+        inplace_reverse(0, len(nums)-1)        
