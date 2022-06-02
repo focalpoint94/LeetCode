7ce13ea8b3ddcd76f018afb1cfedd0a183c8ef43
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-06-02 16:20:03 +0900

    Update 743. Network Delay Time.py

--- 743. Network Delay Time.py
+++ 743. Network Delay Time.py
@@ -1,22 +1,19 @@
+import heapq 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        import heapq
-        INF = int(1e9)
-        distance = [INF] * (n+1)
         graph = [[] for _ in range(n+1)]
-        
         for u, v, w in times:
             graph[u].append((v, w))
-            
-        pq = [(0, k)]
-        distance[k] = 0
-        while pq:
-            dist_u, u = heapq.heappop(pq)
+        distance = [float('inf')] * (n+1)
+        distance[0] = distance[k] = 0
+        
+        h = [(k, 0)]
+        while h:
+            u, dist_u = heapq.heappop(h)
             if dist_u > distance[u]:
                 continue
             for v, dist_uv in graph[u]:
                 if distance[v] > dist_u + dist_uv:
                     distance[v] = dist_u + dist_uv
-                    heapq.heappush(pq, (distance[v], v))
-        
-        return max(distance[1:]) if max(distance[1:]) != INF else -1
+                    heapq.heappush(h, (v, distance[v]))
+        return -1 if max(distance) == float('inf') else max(distance)
