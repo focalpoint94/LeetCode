ca835917e8f52b3bce0269e4d9e9c547e6a4f1a5
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-06-04 09:18:10 +0900

    Update 1631. Path With Minimum Effort.py

--- 1631. Path With Minimum Effort.py
+++ 1631. Path With Minimum Effort.py
@@ -29,39 +29,39 @@ class Solution:
 Binary Search
 '''
 
-# class Solution:
-#     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-#         m, n = len(heights), len(heights[0])
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m, n = len(heights), len(heights[0])
         
-#         def isReachable(budget):
-#             visited = set()
-#             visited.add((0, 0))
-#             q = [(0, 0)]
-#             dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
-#             while q:
-#                 next_q = []
-#                 for y, x in q:
-#                     for d in range(4):
-#                         next_y, next_x = y + dy[d], x + dx[d]
-#                         if 0 <= next_y < m and 0 <= next_x < n:
-#                             if (next_y, next_x) not in visited:
-#                                 cost = abs(heights[next_y][next_x]-heights[y][x])
-#                                 if cost <= budget:
-#                                     visited.add((next_y, next_x))
-#                                     next_q.append((next_y, next_x))
-#                 q = next_q
-#             return (m-1, n-1) in visited
+        def isReachable(budget):
+            visited = set()
+            visited.add((0, 0))
+            q = [(0, 0)]
+            dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
+            while q:
+                next_q = []
+                for y, x in q:
+                    for d in range(4):
+                        next_y, next_x = y + dy[d], x + dx[d]
+                        if 0 <= next_y < m and 0 <= next_x < n:
+                            if (next_y, next_x) not in visited:
+                                cost = abs(heights[next_y][next_x]-heights[y][x])
+                                if cost <= budget:
+                                    visited.add((next_y, next_x))
+                                    next_q.append((next_y, next_x))
+                q = next_q
+            return (m-1, n-1) in visited
         
-#         ret = 10 ** 6
-#         left, right = 0, 10**6
-#         while left <= right:
-#             mid = (left + right) // 2
-#             if isReachable(mid):
-#                 ret = min(ret, mid)
-#                 right = mid - 1
-#             else:
-#                 left = mid + 1
-#         return ret
+        ret = 10 ** 6
+        left, right = 0, 10**6
+        while left <= right:
+            mid = (left + right) // 2
+            if isReachable(mid):
+                ret = min(ret, mid)
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ret
 
 '''
 BFS
