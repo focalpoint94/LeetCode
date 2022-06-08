4afa24cda4581c8094c94cc170a0c23a043deceb
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-06-08 11:29:30 +0900

    Update 76. Minimum Window Substring.py

--- 76. Minimum Window Substring.py
+++ 76. Minimum Window Substring.py
@@ -1,19 +1,22 @@
-from collections import Counter
-class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
-            return ''
-        ret = ''
-        counter, charSet = Counter(t), set(t)        
-        j = 0
-        for i, c in enumerate(s):
-            counter[c] -= 1
-            if c in charSet and counter[c] == 0:
-                charSet.remove(c)
-            while j < i and counter[s[j]] < 0:
-                counter[s[j]] += 1
-                j += 1
-            if not charSet:
-                if not ret or len(ret) > i - j + 1:
-                    ret = s[j:i+1]
-        return ret
+
+
+
+# from collections import Counter
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         if len(s) < len(t):
+#             return ''
+#         ret = ''
+#         counter, charSet = Counter(t), set(t)        
+#         j = 0
+#         for i, c in enumerate(s):
+#             counter[c] -= 1
+#             if c in charSet and counter[c] == 0:
+#                 charSet.remove(c)
+#             while j < i and counter[s[j]] < 0:
+#                 counter[s[j]] += 1
+#                 j += 1
+#             if not charSet:
+#                 if not ret or len(ret) > i - j + 1:
+#                     ret = s[j:i+1]
+#         return ret
