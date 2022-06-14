9dc3ca56bab7832965912ce4228ca74b04fcb6b7
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-06-14 11:25:44 +0900

    Update 210. Course Schedule II.py

--- 210. Course Schedule II.py
+++ 210. Course Schedule II.py
@@ -1,23 +1,19 @@
+from collections import deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        indegrees = [0] * numCourses
         graph = [[] for _ in range(numCourses)]
+        indegrees = [0] * numCourses
         for v, u in prerequisites:
             graph[u].append(v)
             indegrees[v] += 1
-            
-        ret = []
-        q = []
-        for node, indegree in enumerate(indegrees):
-            if indegree == 0:
-                q.append(node)
         
+        ret = []
+        q = deque([u for u in range(numCourses) if indegrees[u] == 0])
         while q:
-            u = q.pop()
+            u = q.popleft()
             ret.append(u)
             for v in graph[u]:
                 indegrees[v] -= 1
                 if indegrees[v] == 0:
                     q.append(v)
-        
         return ret if len(ret) == numCourses else []
