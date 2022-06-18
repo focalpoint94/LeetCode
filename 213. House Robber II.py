0e28d2b12f10d3a9ea6f9bf8e847cfca31f0dc72
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-06-18 16:26:38 +0900

    Update 213. House Robber II.py

--- 213. House Robber II.py
+++ 213. House Robber II.py
@@ -4,15 +4,16 @@ class Solution:
             return nums[0]
         if len(nums) == 2:
             return max(nums[0], nums[1])
-        dp = [0] * (len(nums) - 1)
+        N = len(nums)
+        dp = [0] * (N - 1)
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums)-1):
+        for i in range(2, N-1):
             dp[i] = max(dp[i-2]+nums[i], dp[i-1])
         cand1 = dp[-1]
-        dp = [0] * (len(nums) - 1)
+        dp = [0] * (N - 1)
         dp[0] = nums[1]
         dp[1] = max(nums[1], nums[2])
-        for i in range(2, len(nums)-1):
+        for i in range(2, N-1):
             dp[i] = max(dp[i-2]+nums[i+1], dp[i-1])
         return max(cand1, dp[-1])
