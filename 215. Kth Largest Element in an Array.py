362fa9efd0a4ed7474cb5bcc5eb1b30b081af34a
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-06-19 09:36:29 +0900

    Update 215. Kth Largest Element in an Array.py

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -10,27 +10,27 @@ class Solution:
                 heapq.heappop(h)
         return h[0]
 
-# class Solution:
-#     def findKthLargest(self, nums: List[int], k: int) -> int:
-#         k = len(nums) - k
-#         low, high = 0, len(nums) - 1
-#         while low < high:
-#             j = self.partition(nums, low, high)
-#             if j < k:
-#                 low = j + 1
-#             elif j > k:
-#                 high = j - 1
-#             else:
-#                 return nums[k]
-#         return nums[low]
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def partition(left, right):
+            rand_idx = random.randint(left, right)
+            nums[rand_idx], nums[right] = nums[right], nums[rand_idx]
+            pivot, j = nums[right], left
+            for i in range(left, right):
+                if nums[i] <= pivot:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    j += 1
+            nums[j], nums[right] = nums[right], nums[j]
+            return j
         
-#     def partition(self, nums, low, high):
-#         rand_idx = random.randint(low, high)
-#         nums[rand_idx], nums[high] = nums[high], nums[rand_idx]
-#         pivot, left = nums[high], low
-#         for i in range(low, high):
-#             if nums[i] <= pivot:
-#                 nums[i], nums[left] = nums[left], nums[i]
-#                 left += 1
-#         nums[left], nums[high] = nums[high], nums[left]
-#         return left
+        k = len(nums) - k
+        left, right = 0, len(nums) - 1
+        # left <= k <= right
+        while left <= right:
+            j = partition(left, right)
+            if j > k:
+                right = j - 1
+            elif j < k:
+                left = j + 1
+            else:
+                return nums[k]
