3ed79df8ddd463f8330f0a87f4ec78542d122818
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-06-29 13:48:53 +0900

    Update 239. Sliding Window Maximum.py

--- 239. Sliding Window Maximum.py
+++ 239. Sliding Window Maximum.py
@@ -1,14 +1,14 @@
 from collections import deque
-
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         ret = []
-        # q stores index
         q = deque()
-        for i in range(len(nums)):
-            while q and nums[q[-1]] <= nums[i]:
+        for i, num in enumerate(nums):
+            # monotonicly descending deque (storing index)    
+            while q and nums[q[-1]] <= num:
                 q.pop()
             q.append(i)
+            # if expired
             if q[0] == i - k:
                 q.popleft()
             if i >= k - 1:
