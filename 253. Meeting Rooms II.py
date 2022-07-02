6060896ce0d1ea4d17e5c888d73b075d20692881
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-07-02 09:50:22 +0900

    Update 253. Meeting Rooms II.py

--- 253. Meeting Rooms II.py
+++ 253. Meeting Rooms II.py
@@ -1,12 +1,12 @@
 import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        h = []
         ret = 0
-        for interval in intervals:
-            while h and h[0] <= interval[0]:
-                heapq.heappop(h)
-            heapq.heappush(h, interval[1])
-            ret = max(ret, len(h))
+        intervals.sort()
+        q = []
+        for s, e in intervals:
+            while q and q[0] <= s:
+                heapq.heappop(q)
+            heapq.heappush(q, e)
+            ret = max(ret, len(q))
         return ret
