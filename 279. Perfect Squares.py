b89f52b97da3644227fee9f57a4229125be1e50d
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-07-09 12:40:07 +0900

    Update 279. Perfect Squares.py

--- 279. Perfect Squares.py
+++ 279. Perfect Squares.py
@@ -13,3 +13,18 @@ class Solution:
         for h in range(1, n+1):
             if iddfs(n, h):
                 return h
+            
+            
+from collections import deque
+class Solution:
+    def numSquares(self, n: int) -> int:
+        PSNs = set([i*i for i in range(1, int(n**0.5)+1)])
+        q = deque()
+        q.append((n, 0))
+        while q:
+            n, t = q.popleft()
+            for PSN in PSNs:
+                if PSN == n:
+                    return t + 1
+                if PSN < n:
+                    q.append((n-PSN, t+1))
