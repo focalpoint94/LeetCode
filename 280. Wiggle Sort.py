5b375c28fc63793b60fff309dd274485bd7f0653
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-07-10 10:45:21 +0900

    Update 280. Wiggle Sort.py

--- 280. Wiggle Sort.py
+++ 280. Wiggle Sort.py
@@ -3,12 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        big = False
-        for i in range(len(nums)-1):
-            if big:
+        biggerThanNext = False
+        for i in range(len(nums) - 1):
+            if biggerThanNext:
                 if nums[i] < nums[i+1]:
                     nums[i], nums[i+1] = nums[i+1], nums[i]
             else:
                 if nums[i] > nums[i+1]:
                     nums[i], nums[i+1] = nums[i+1], nums[i]
-            big = not big
+            biggerThanNext =
