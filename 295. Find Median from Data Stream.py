dcc858df1ffb50408f4af70185c0a0e9c98d6cb6
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-07-16 13:53:13 -0700

    Update 295. Find Median from Data Stream.py

--- 295. Find Median from Data Stream.py
+++ 295. Find Median from Data Stream.py
@@ -1,27 +1,30 @@
+import heapq
 class MedianFinder:
-    import heapq
-    
-    def __init__(self):
-        self.h1 = []
-        self.h2 = []
-        self.count = 0
 
+    def __init__(self):
+        self.size = 0
+        # self.h1: max heap containing the small half
+        # self.h2: min heap containing the large half
+        self.h1, self.h2 = [], []
+    
     def addNum(self, num: int) -> None:
-        self.count += 1
-        if self.count % 2 == 1:
+        self.size += 1
+        if self.size % 2 == 1:
             if not self.h1 or num <= self.h2[0]:
                 heapq.heappush(self.h1, -num)
             else:
                 heapq.heappush(self.h1, -heapq.heappushpop(self.h2, num))
         else:
-            if num >= -self.h1[0]:
-                heapq.heappush(self.h2, num)
+            if num <= -self.h1[0]:
+                heapq.heappush(self.h2, -heapq.heappushpop(self.h1, -num))
             else:
-                heapq.heappush(self.h2, -heapq.heappushpop(self.h1, -num))            
+                heapq.heappush(self.h2, num)
+
     def findMedian(self) -> float:
-        if self.count % 2 == 1:
+        if self.size % 2 == 0:
+            return (-self.h1[0] + self.h2[0])/2.
+        else:
             return -self.h1[0]
-        return (-self.h1[0] + self.h2[0])/2.   
 
 
 # Your MedianFinder object will be instantiated and called as such:
