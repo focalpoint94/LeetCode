97b96611c8216938fb4ae777633b3aca3d2d3a51
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-07-30 13:18:15 -0500

    Update 495. Teemo Attacking.py

--- 495. Teemo Attacking.py
+++ 495. Teemo Attacking.py
@@ -1,10 +1,7 @@
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
-        ret, covered = 0, -1
-        for t in timeSeries:
-            if covered < t:
-                ret += duration
-            else:
-                ret += max(0, t + duration - 1 - covered)
-            covered = max(covered, t + duration - 1)
+        ret = 0
+        for i in range(1, len(timeSeries)):
+            ret += min(timeSeries[i]-timeSeries[i-1], duration)
+        ret += duration
         return ret
