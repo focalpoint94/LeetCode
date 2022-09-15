0edc5066e1aefa3f1abafee0c4de4f27408d1efa
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-09-14 21:11:44 -0400

    Update 833. Find And Replace in String.py

--- 833. Find And Replace in String.py
+++ 833. Find And Replace in String.py
@@ -1,20 +1,11 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        # [index, source, target]
-        matched = []
-        k = len(indices)
-        for i in range(k):
-            idx, source, target = indices[i], sources[i], targets[i]
-            # match?
-            if s[idx:idx+len(source)] == source:
-                matched.append((idx, source, target))
-        
-        matched.sort()
-        strList, prv = [], 0
-        for idx, source, target in matched:
-            strList.append(s[prv:idx])
-            strList.append(target)
-            prv = idx + len(source)
-        strList.append(s[prv:])
-        
-        return ''.join(strList)
+        ret = [c for c in s]
+        temp = [(indice, source, target) for (indice, source, target) in sorted(zip(indices, sources, targets), reverse=True)]
+        indices = [x[0] for x in temp]
+        sources = [x[1] for x in temp]
+        targets = [x[2] for x in temp]
+        for i, index in enumerate(indices):
+            if sources[i] == s[index:index+len(sources[i])]:
+                ret[index:index+len(sources[i])] = [c for c in targets[i]]
+        return ''.join(ret)
