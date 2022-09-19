ca77b5ffd34ecf84c54b81124251897e8fcab96f
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-09-19 13:44:32 -0400

    Update 968. Binary Tree Cameras.py

--- 968. Binary Tree Cameras.py
+++ 968. Binary Tree Cameras.py
@@ -6,36 +6,22 @@
 #         self.right = right
 class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
-        self.ret = 0
-        if self.dfs(root) == 'to be covered':
-            self.ret += 1
-        return self.ret
+        ret = 0
         
-    def dfs(self, node):
-        if not node.left and not node.right:
-            return 'to be covered'
-        if node.left and not node.right:
-            left = self.dfs(node.left)
-            if left == 'covered':
-                return 'to be covered'
-            if left == 'cam':
-                return 'covered'
-            if left == 'to be covered':
-                self.ret += 1
-                return 'cam'
-        if not node.left and node.right:
-            right = self.dfs(node.right)
-            if right == 'covered':
-                return 'to be covered'
-            if right == 'cam':
-                return 'covered'
-            if right == 'to be covered':
-                self.ret += 1
-                return 'cam'
-        left, right = self.dfs(node.left), self.dfs(node.right)
-        if left == 'to be covered' or right == 'to be covered':
-            self.ret += 1
-            return 'cam'
-        if left == 'cam' or right == 'cam':
-            return 'covered'
-        return 'to be covered'
+        def dfs(node):
+            if not node:
+                return 'watched'
+            nonlocal ret
+            left, right = dfs(node.left), dfs(node.right)
+            if left == 'to be watched' or right == 'to be watched':
+                ret += 1
+                return 'camera'
+            if left == 'camera' or right == 'camera':
+                return 'watched'
+            if left == 'watched' and right == 'watched':
+                return 'to be watched'
+
+        state = dfs(root)
+        if state == 'to be watched':
+            ret += 1
+        return ret
