4b61262ca8d892476e0e53f2a94f60fdf32a6449
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-09-23 10:47:29 -0400

    Update 388. Longest Absolute File Path.py

--- 388. Longest Absolute File Path.py
+++ 388. Longest Absolute File Path.py
@@ -1,18 +1,17 @@
 class Solution:
     def lengthLongestPath(self, input: str) -> int:
-        inp = input.split('\n') 
+        inp = input.split('\n')
         ret = 0
         stack = [0]
-        for i in range(0, len(inp)):
-            address = inp[i]
-            cnt = address.count('\t')
-            name = address[cnt:]
-            if cnt == 0:
+        for address in inp:
+            lvl = address.count('\t')
+            name = address.split('\t')[-1]
+            if lvl == 0:
                 stack[0] = len(name)
-            elif cnt == len(stack):
+            elif lvl == len(stack):
                 stack.append(stack[-1]+len(name)+1)
             else:
-                stack[cnt] = stack[cnt-1]+len(name)+1
+                stack[lvl] = stack[lvl-1]+len(name)+1
             if '.' in name:
-                ret = max(ret, stack[cnt])
+                ret = max(ret, stack[lvl])
         return ret
