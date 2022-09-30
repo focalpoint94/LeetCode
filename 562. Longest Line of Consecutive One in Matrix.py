b4c6147eea830de98775752d9e8419a807b34933
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-09-30 14:08:37 -0400

    Update 562. Longest Line of Consecutive One in Matrix.py

--- 562. Longest Line of Consecutive One in Matrix.py
+++ 562. Longest Line of Consecutive One in Matrix.py
@@ -1,29 +1,35 @@
+from functools import lru_cache
 class Solution:
     def longestLine(self, mat: List[List[int]]) -> int:
         m, n = len(mat), len(mat[0])
-        self.mat = mat
-        self.dp = [[[0] * 8 for col in range(n)] for row in range(m)]
-        self.ret = 0
+        dp = {}
+        for y in range(m):
+            for x in range(n):
+                dp[(y, x)] = [False] * 8
         
-        dys, dxs = [0, 1, 1, 1, 0, -1, -1, -1], [1, 1, 0, -1, -1, -1, 0, 1]
+        ret = 0
+        dy, dx = [0, 1, 1, 1, 0, -1, -1, -1], [1, 1, 0, -1, -1, -1, 0, 1]
+        
+        def dfs(y, x, d):
+            nonlocal m, n, ret
+            if dp[(y, x)][d] != False:
+                return dp[(y, x)][d]
+            
+            if mat[y][x] == 0:
+                dp[(y, x)][d] = 0
+                return 0
+            
+            next_y, next_x = y + dy[d], x + dx[d]
+            if 0 <= next_y < m and 0 <= next_x < n:
+                dp[(y, x)][d] = dfs(next_y, next_x, d) + 1
+            else:
+                dp[(y, x)][d] = 1
+            ret = max(ret, dp[(y, x)][d])
+            return dp[(y, x)][d]
+            
         for y in range(m):
             for x in range(n):
                 for d in range(8):
-                    dy, dx = dys[d], dxs[d]
-                    if self.mat[y][x] == 1 and not self.dp[y][x][d]:
-                        self.dfs(y, x, d, dy, dx)
-        return self.ret
-        
-    def dfs(self, y, x, d, dy, dx):
-        if y < 0 or y >= len(self.mat) or x < 0 or x >= len(self.mat[0]) or \
-            self.mat[y][x] == 0:
-            return 0
-        
-        if self.dp[y][x][d] != 0:
-            return self.dp[y][x][d]
-        
-        next_y, next_x = y + dy, x + dx
-        ret = self.dfs(next_y, next_x, d, dy, dx) + 1
-        self.dp[y][x][d] = ret
-        self.ret = max(self.ret, ret)
+                    dfs(y, x, d)
+                    
         return ret
