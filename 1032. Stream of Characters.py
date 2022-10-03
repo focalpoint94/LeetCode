e7a8293418ee3be861c36c988645b818a52279c4
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-10-03 11:23:46 -0400

    Update 1032. Stream of Characters.py

--- 1032. Stream of Characters.py
+++ 1032. Stream of Characters.py
@@ -1,31 +1,29 @@
 from collections import deque
-
 class StreamChecker:
 
     def __init__(self, words: List[str]):
+        self.maxLength = 0
         self.trie = {}
         for word in words:
-            node = self.trie
+            t = self.trie
             for char in word[::-1]:
-                if char not in node:
-                    node[char] = {}
-                node = node[char]
-            node['#'] = '#'
-        self.q = deque(maxlen=2000)
+                if char not in t:
+                    t[char] = {}
+                t = t[char]
+            t['#'] = '#'
+            self.maxLength = max(self.maxLength, len(word))
+        self.q = deque(maxlen = self.maxLength)
 
     def query(self, letter: str) -> bool:
-        self.q.append(letter)
-        node = self.trie
-        for i in range(len(self.q)-1, -1, -1):
-            letter = self.q[i]
-            if letter not in node:
-                return False
-            node = node[letter]
-            if '#' in node:
+        self.q.appendleft(letter)
+        t = self.trie
+        for l in self.q:
+            if '#' in t:
                 return True
-        return False
-        
-        
+            if l not in t:
+                return False
+            t = t[l]
+        return '#' in t
 
 
 # Your StreamChecker object will be instantiated and called as such:
