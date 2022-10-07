a184820cb811893c30eb9e0eca05bade101c984d
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-10-07 12:17:01 -0400

    Update 1293. Shortest Path in a Grid with Obstacles Elimination.py

--- 1293. Shortest Path in a Grid with Obstacles Elimination.py
+++ 1293. Shortest Path in a Grid with Obstacles Elimination.py
@@ -1,37 +1,43 @@
+from collections import deque
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        m, n = len(grid) - 1, len(grid[0]) - 1
+        m, n = len(grid), len(grid[0])
         dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
-        q = set()
-        q.add((0, 0, k))
-        dp = {}
-        dp[(0, 0)] = 0
-        step = 0
+        
+        q = deque()
+        q.append((0, 0, k, 0))
+        
+        visited = {}
+        # point: t
+        visited[(0, 0)] = k
+        
         while q:
-            next_q = set()
-            for y, x, i in q:
-                # if reached the goal:
-                if y == m and x == n:
-                    return step
-                for d in range(4):
-                    next_y, next_x = y + dy[d], x + dx[d]
-                    # if next point is valid:
-                    if 0 <= next_y <= m and 0 <= next_x <= n:
-                        # if next point is empty:
+            y, x, t, l = q.popleft()
+            # arrived
+            if y == m - 1 and x == n - 1:
+                return l
+            for d in range(4):
+                next_y, next_x = y + dy[d], x + dx[d]
+                # within grid
+                if 0 <= next_y < m and 0 <= next_x < n:
+                    # never visited
+                    if (next_y, next_x) not in visited:
+                        visited[(next_y, next_x)] = t
+                        # empty
                         if grid[next_y][next_x] == 0:
-                            # if next point is not visited or it is efficient:
-                            if (next_y, next_x) not in dp or \
-                                i > dp[(next_y, next_x)]:
-                                dp[(next_y, next_x)] = i
-                                next_q.add((next_y, next_x, i))
-                        # if next point is wall:
+                            q.append((next_y, next_x, t, l+1))
                         else:
-                            # if next point is not visited or it is efficient:
-                            if (next_y, next_x) not in dp or \
-                                i - 1 > dp[(next_y, next_x)]:
-                                if i >= 1:
-                                    dp[(next_y, next_x)] = i - 1
-                                    next_q.add((next_y, next_x, i-1))
-            q = next_q
-            step += 1
+                            if t >= 1:
+                                q.append((next_y, next_x, t-1, l+1))
+                    # if visited
+                    else:
+                        if t > visited[(next_y, next_x)]:
+                            visited[(next_y, next_x)] = t
+                            # empty
+                            if grid[next_y][next_x] == 0:
+                                q.append((next_y, next_x, t, l+1))
+                            else:
+                                if t >= 1:
+                                    q.append((next_y, next_x, t-1, l+1))
         return -1
+        
