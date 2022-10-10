647fd5d20e31603d5f8bf10d7a092328d5aab915
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-10-10 13:15:39 -0400

    Update 1423. Maximum Points You Can Obtain from Cards.py

--- 1423. Maximum Points You Can Obtain from Cards.py
+++ 1423. Maximum Points You Can Obtain from Cards.py
@@ -1,19 +1,12 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        n = len(cardPoints)
-        if k >= n:
-            return sum(cardPoints)
-
-        sums = [0] * (n+1)
-        cul = 0
-        for idx, card in enumerate(cardPoints):
-            cul += card
-            sums[idx+1] = cul
-        def getSum(l, r):
-            return sums[l] - sums[0] + sums[-1] - sums[-1-r]
+        N = len(cardPoints)
+        prefixSum = [0]
+        for point in cardPoints:
+            prefixSum.append(prefixSum[-1] + point)
         
-        ret = 0
-        for l in range(k+1):
-            r = k - l
-            ret = max(ret, getSum(l, r))
+        ret = -1
+        for i in range(0, k+1):
+            j = k - i
+            ret = max(ret, prefixSum[i]-prefixSum[0]+prefixSum[N]-prefixSum[N-j])
         return ret
