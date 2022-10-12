2022a3fee287e777161d2d9ba3705f1e1825850e
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2022-10-12 13:50:41 -0400

    Update 1834. Single-Threaded CPU.py

--- 1834. Single-Threaded CPU.py
+++ 1834. Single-Threaded CPU.py
@@ -1,33 +1,29 @@
+import heapq
+from collections import deque
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        import heapq
+        N = len(tasks)
         ret = []
+        tasks = [(task[0], task[1], idx) for idx, task in enumerate(tasks)]
+        tasks.sort()
+        tasks = deque(tasks)
         
-        TaskHeap = []
-        WaitHeap = []
-        
-        for idx, task in enumerate(tasks):
-            heapq.heappush(TaskHeap, (task[0], idx, task[1]))
-            
-        curTime = 0
-        while True:
-            
-            if not WaitHeap:
-                if not TaskHeap:
-                    break                
-                startTime, idx, processTime = heapq.heappop(TaskHeap)
-                heapq.heappush(WaitHeap, (processTime, idx, startTime))
-                while TaskHeap and TaskHeap[0][0] == startTime:
-                    startTime, idx, processTime = heapq.heappop(TaskHeap)
-                    heapq.heappush(WaitHeap, (processTime, idx, startTime))
-                curTime = startTime
-            
-            processTime, idx, startTime = heapq.heappop(WaitHeap)
-            ret.append(idx)
-            curTime += processTime
-            
-            while TaskHeap and TaskHeap[0][0] <= curTime:
-                startTime, idx, processTime = heapq.heappop(TaskHeap)
-                heapq.heappush(WaitHeap, (processTime, idx, startTime))
-        
+        # q for cpu
+        # (processingTime, idx)
+        q = []
+        t = 0
+        while len(ret) < N:
+            while tasks:
+                enqueueTime, processingTime, idx = tasks.popleft()
+                if enqueueTime <= t:
+                    heapq.heappush(q, (processingTime, idx))
+                else:
+                    tasks.appendleft((enqueueTime, processingTime, idx))
+                    break
+            if q:
+                processingTime, idx = heapq.heappop(q)
+                t += processingTime
+                ret.append(idx)
+            else:
+                t = tasks[0][0]
         return ret
